a7fd4ff24a79bfc72c12c41f769a260577d60105
Michel Feldheim <m.feldheim@opendi.com>
2026-05-09 02:17:49 +0200

    feat: add CLI entrypoint with leader election and engine wiring

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,120 @@
-// Package cmd will contain the Klyra CLI (implemented in a later task).
+// cmd/root.go
 package cmd
 
-// Execute is a placeholder that will be implemented in a later task.
-func Execute() {}
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	_ "github.com/mfeldheim/klyra/internal/action/http"
+	_ "github.com/mfeldheim/klyra/internal/monitor/http"
+	_ "github.com/mfeldheim/klyra/internal/monitor/kubernetes"
+	_ "github.com/mfeldheim/klyra/internal/monitor/prometheus"
+
+	"github.com/mfeldheim/klyra/internal/config"
+	"github.com/mfeldheim/klyra/internal/engine"
+	"github.com/mfeldheim/klyra/internal/leader"
+	"github.com/mfeldheim/klyra/internal/server"
+	"github.com/mfeldheim/klyra/internal/state"
+)
+
+var (
+	flagConfigPath string
+	flagAddr       string
+	flagNamespace  string
+	flagLeaseName  string
+	flagKubeconfig string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "klyra",
+	Short: "Kubernetes monitoring tool",
+	RunE:  run,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().StringVar(&flagConfigPath, "config", "/etc/klyra/config.yaml", "path to config file")
+	rootCmd.Flags().StringVar(&flagAddr, "addr", ":8080", "HTTP listen address")
+	rootCmd.Flags().StringVar(&flagNamespace, "namespace", "default", "Kubernetes namespace")
+	rootCmd.Flags().StringVar(&flagLeaseName, "lease-name", "klyra-leader", "leader election lease name")
+	rootCmd.Flags().StringVar(&flagKubeconfig, "kubeconfig", "", "path to kubeconfig (empty = in-cluster)")
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	f, err := os.Open(flagConfigPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	cfg, err := config.Load(f)
+	if err != nil {
+		return err
+	}
+
+	k8sClient, err := buildK8sClient(flagKubeconfig)
+	if err != nil {
+		return err
+	}
+
+	st := state.NewStore()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	if err := engine.LoadFromConfigMap(ctx, st, k8sClient, flagNamespace, "klyra-state"); err != nil {
+		log.Printf("warning: could not load persisted state: %v", err)
+	}
+
+	h := server.NewHandlers(st, cfg)
+	srv := server.New(h, nil) // UI fs injected at build time via embed.go
+	go func() {
+		if err := srv.ListenAndServe(flagAddr); err != nil {
+			log.Printf("server error: %v", err)
+		}
+	}()
+
+	eng, err := engine.New(cfg, st, k8sClient, flagNamespace)
+	if err != nil {
+		return err
+	}
+
+	leader.Run(ctx, k8sClient, flagNamespace, flagLeaseName,
+		func(leaderCtx context.Context) {
+			log.Println("leader: starting engine")
+			if err := eng.Run(leaderCtx); err != nil {
+				log.Printf("engine error: %v", err)
+			}
+		},
+		func() { log.Println("leader: engine stopped") },
+	)
+
+	return nil
+}
+
+func buildK8sClient(kubeconfig string) (kubernetes.Interface, error) {
+	var restCfg *rest.Config
+	var err error
+	if kubeconfig != "" {
+		restCfg, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	} else {
+		restCfg, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(restCfg)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/spf13/cobra v1.10.2
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/api v0.31.0
 	k8s.io/apimachinery v0.31.0
@@ -23,6 +24,8 @@ require (
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -30,6 +33,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
